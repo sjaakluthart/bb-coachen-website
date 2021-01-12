ef8c8c1025b6c41113ad99a62db44c94a6fc8b0b
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-12 21:53:54 +0100

    Changed to hash router.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,14 +7,14 @@ import Home from '../components/Home';
 import About from '../components/About';
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Switch>
       <Route path="/about" component={About} />
       <Route path="/" component={Home} />
     </Switch>
     <Footer />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
