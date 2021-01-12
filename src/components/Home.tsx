9831f69123642f7721051432ad5a67faf7c323c3
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-12 17:03:37 +0100

    Initial commit.

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <main>
+    <h1>Home goes here</h1>
+  </main>
+);
+
+export default Home;
---
+++ src/router/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from '../components/Home';
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
