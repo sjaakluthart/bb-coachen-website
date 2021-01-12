a9137227336c002e96eedddb1d95db43d14433c9
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-12 21:56:20 +0100

    Revert changes.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,14 +7,14 @@ import Home from '../components/Home';
 import About from '../components/About';
 
 const Router = () => (
-  <HashRouter>
+  <BrowserRouter>
     <Header />
     <Switch>
       <Route path="/about" component={About} />
       <Route path="/" component={Home} />
     </Switch>
     <Footer />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Router;
