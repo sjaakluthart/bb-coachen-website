543b33e73197eca673be077900f1c8f9055ff964
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-12 21:19:15 +0100

    Added text.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => (
       <article>
         <h1>Bart Boogaard, Buitencoach</h1>
         <p>
-          Je bent een ondernemende jongvolwassene of (top)sporter. Je wilt je doelen bereiken. In je werk, studie of sport én in je leven. Maar hebt niet helder waarom je niet verder komt. Of wat je het allerliefste wilt. Wat nu?
+          Je bent een ondernemende jongvolwassene of (top)sporter. Je wilt je doelen bereiken. In je werk, studie of sport én in je leven. Maar je hebt niet helder waarom je niet verder komt. Of wat je het allerliefste wilt. Wat nu?
         </p>
         <p>
           Ik ga met jou op zoek naar jouw focus, wat voor jou werkt en naar jouw glimlach.
@@ -59,10 +59,7 @@ const Home = () => (
           Ik begeleid al meer dan tien jaar mensen die het beste uit zichzelf en het leven willen halen. Die meer zelfvertrouwen en eigenaarschap willen. Vooral jongvolwassenen die willen excelleren in hun leven, werk, studie of sport. Of gewoon weer plezier willen hebben in wat ze doen.
         </p>
         <p>
-          Met rust luister ik, confronteer ik en laat ik je voelen.
-        </p>
-        <p>
-          We gaan naar buiten, met elkaar in gesprek en doen fysieke oefeningen.
+          Met rust luister ik, confronteer ik en laat ik je voelen. We gaan naar buiten, met elkaar in gesprek en doen fysieke oefeningen.
         </p>
         <p>
           Door die ervaringen ga je anders kijken naar jezelf en je omgeving. En ontdek je stappen die je tot dan toe niet zag.
@@ -76,10 +73,7 @@ const Home = () => (
           Om jouw persoonlijke traject vorm te geven, tap ik uit mijn ervaring als mental coach en mijn coachopleiding, aanvullende opleidingen en workshops. En natuurlijk uit mijn eigen sportervaring en mijn persoonlijke ontwikkelingspad.
         </p>
         <p>
-          We gaan samen op weg, met aandacht voor wat nodig is.
-        </p>
-        <p>
-          Ik laat je ervaren en nieuwe wegen ontdekken, maar nooit verdwalen.
+          We gaan samen op weg, met aandacht voor wat nodig is. Ik laat je ervaren en nieuwe wegen ontdekken, maar nooit verdwalen.
         </p>
       </section>
     </article>
@@ -88,6 +82,22 @@ const Home = () => (
         <h1>Brandt jouw vuur nog?</h1>
       </article>
     </section>
+    <article>
+      <section>
+        <h1>Hoe ziet een Coach Expeditie eruit?</h1>
+        <p>
+          Een Coach Expeditie start altijd met een kosteloos oriënterend gesprek. Ik luister en jij vertelt wat je zoekt en welke tijd- en financiële investering je kunt bieden. Op basis daarvan bespreken we de lengte en vorm van de Coach Expeditie die past bij jou.
+        </p>
+        <p>
+          Als jij beslist om met mij in zee te gaan, starten we het eerste kwartaal intensief en houd je jouw ervaringen bij in een digitaal logboek. Vanaf het tweede kwartaal is de intensiteit lager. Tijdens jouw Coach Expeditie ben ik bereikbaar voor afstemming, dat helpt jouw proces en ondersteunt het vertrouwen.
+        </p>
+      </section>
+      <section>
+        <p>
+          Gemiddeld duurt een Coach Expeditie twee tot vier kwartalen. Qua kosten voor een expeditie kun je denken aan circa €800 tot €1.400 inclusief btw. (voor bedrijven is dit exclusief btw)
+        </p>
+      </section>
+    </article>
   </main>
 );
 
