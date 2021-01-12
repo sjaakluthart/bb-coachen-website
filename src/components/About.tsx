9ef6050d0da54b5d38b819d6596476c775307159
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-12 20:17:45 +0100

    Added About page.

---
+++ src/components/About.tsx
@@ -0,0 +1,52 @@
+/* eslint max-len: 0 */
+import React from 'react';
+
+import '../css/About.css';
+
+const About = () => (
+  <main className="page about">
+    <section className="hero">
+      <article>
+        <h1>Wie is Bart?</h1>
+        <p>
+          Bart is een nuchtere en aimabele levenskunstenaar met empathie en confronteert waar nodig. Als financieel professional keek hij al naar de mens achter de cijfers, als buitencoach werd dat zijn vak.
+        </p>
+      </article>
+    </section>
+    <article>
+      <section>
+        <h1>Bart Boogaard</h1>
+        <p>
+          Ik ben opgegroeid aan de Noord-Hollandse kust en was altijd veel buiten. Ik hield en houd van buitenspelen. Dit gebruik ik ook actief in mijn coaching. Buiten geeft beweging en ruimte.
+        </p>
+        <p>
+          Tijdens mijn diensttijd ontpopte ik mij als marinier tot de natuurlijke leider van mijn opleidingsgroep. Meer grenzen verlegde ik op het ijs, in de bergen en golven en op of langs het voetbal- en rugbyveld. Sporten en bewegen hebben mij gevormd als mens en coach.
+        </p>
+        <p>
+          Ervaringen die ik verbind met mijn jarenlange training in tai chi, waarmee actie en souplesse samenkomen.
+        </p>
+        <p>
+          Ik werk als financieel professional en begeleidde al jaren ambitieuze sporters. Inmiddels heb ik al heel wat mensen begeleid die verder wilden komen op sportief gebied en in het leven. Zo heb ik een professioneel mountainbiketeam mentaal begeleid en vele (top)sporters individueel en (top)sportteams.
+        </p>
+      </section>
+      <section>
+        <p>
+          Met deze kennis en vaardigheden help ik ook ondernemende jongvolwassenen weer op weg in studie, werk of hun passie.
+        </p>
+        <p>
+          Ik ben altijd bezig om mijzelf te verrijken met nieuwe wijsheid. Ik lees, zoek ervaringen op, volg opleidingen en ga de werkelijke ontmoeting nooit uit de weg. Daarmee blijf ik mijzelf ontwikkelen als mens en coach. Nog altijd zit ik met plezier op de racefiets of mountainbike, loop ik lekker hard of rij ik naar de kust voor een duik in het zoute zeewater. Meestal begin ik dag met een wandeling, die ik afsluit met wat meditatie en tai chi. Daarna kan de dag echt beginnen.
+        </p>
+        <p>
+          Dit alles maakt mij een gedreven en levenslustig mens, met rust en aandacht voor wie behoefte heeft aan inzicht, mentale veerkracht of het terugvinden van een glimlach.
+        </p>
+      </section>
+    </article>
+    <section className="quote bike">
+      <article>
+        <h1>Je best doen is mooi<br />maar je kunt beter het beste doen</h1>
+      </article>
+    </section>
+  </main>
+);
+
+export default About;
