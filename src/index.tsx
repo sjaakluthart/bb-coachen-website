84ad482f16bbfdee69e2d8dd94815712ae9d0a0a
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-13 09:55:08 +0100

    Added react-slick.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import './css/MeyerReset.css';
 import './css/index.css';
