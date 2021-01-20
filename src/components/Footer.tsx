f51ff312a3307adbddef1ccaf92408ee1d1c0d82
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-20 17:19:46 +0100

    Added contact details to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import '../css/Footer.css';
+import home from '../assets/home.svg';
+import phone from '../assets/phone.svg';
+import mail from '../assets/mail.svg';
 
 const Footer = () => (
   <footer>
-    <nav>
-      <NavLink to="/contact">
-        Contact
-      </NavLink>
-      &middot;
+    <section className="contact">
+      <div>
+        <img src={home} alt="home" />
+        <p>Sneeuwvlerk 18, 8802DB Franeker</p>
+      </div>
+      <div>
+        <img src={phone} alt="phone" />
+        <a href="tel:0613540037">06 13 54 00 37</a>
+      </div>
+      <div>
+        <img src={mail} alt="mail" />
+        <a href="mailto:bart@bbcoachen.nl?subject=Coach Expeditie">bart@bbcoachen.nl</a>
+      </div>
+    </section>
+    {/* <nav>
       <NavLink to="/privacy">
         Privacyverklaring
       </NavLink>
@@ -21,7 +34,7 @@ const Footer = () => (
       <NavLink to="/cookies">
         Cookies
       </NavLink>
-    </nav>
+    </nav> */}
     <p>&copy; Copyright BB Coachen {new Date().getFullYear()} &middot; Realisatie <a href="https://sjaakluthart.nl" target="_blank" rel="noreferrer">Sjaak Luthart</a></p>
   </footer>
 );
