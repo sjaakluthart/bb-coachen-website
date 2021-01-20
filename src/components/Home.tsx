025b7b3ae052880898e11f6367dfa93d9deab531
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-20 16:12:22 +0100

    Changed paragraph to list.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -88,15 +88,27 @@ const Home = () => (
           <p>
             Een Coach Expeditie start altijd met een kosteloos oriënterend gesprek. Ik luister en jij vertelt wat je zoekt en welke tijd- en financiële investering je kunt bieden. Op basis daarvan bespreken we de lengte en vorm van de Coach Expeditie die past bij jou.
           </p>
-          <p>
-            Als jij beslist om met mij in zee te gaan, starten we het eerste kwartaal intensief en houd je jouw ervaringen bij in een digitaal logboek. Vanaf het tweede kwartaal is de intensiteit lager. Tijdens jouw Coach Expeditie ben ik bereikbaar voor afstemming, dat helpt jouw proces en ondersteunt het vertrouwen.
-          </p>
+          <ul>
+            <h1>Een Coach Expeditie bestaat uit:</h1>
+            <li>
+              coaching per kwartaal
+            </li>
+            <li>
+              het bijhouden van een digitaal logboek
+            </li>
+            <li>
+              bereikbaarheid voor afstemming met mij als coach, dat helpt jouw proces en ondersteunt het vertrouwen
+            </li>
+            <li>
+              hoge intensiteit in het eerste kwartaal
+            </li>
+          </ul>
         </section>
         <section>
           <p>
             Gemiddeld duurt een Coach Expeditie twee tot vier kwartalen. Qua kosten voor een expeditie kun je denken aan circa €800 tot €1.400 inclusief btw. (voor bedrijven is dit exclusief btw)
           </p>
-          <a href="mailto:bart@bbcoachen.nl?subject=Coach Expeditie" className="sign-up">Meld je aan!</a>
+          <a href="mailto:bart@bbcoachen.nl?subject=Coach Expeditie" className="sign-up button">Meld je aan</a>
         </section>
       </section>
     </article>
