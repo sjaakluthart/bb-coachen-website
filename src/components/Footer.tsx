348584c0099564e0b05cbc5bb607e0212d436900
Sjaak Luthart <mail@sjaakluthart.nl>
2021-01-27 14:26:41 +0100

    Comment line.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 import '../css/Footer.css';
 import home from '../assets/home.svg';
