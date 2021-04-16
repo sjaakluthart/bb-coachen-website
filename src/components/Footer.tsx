c0e55e7fa4768b17c1b7299981ab6c19c9c6e152
Sjaak Luthart <mail@sjaakluthart.nl>
2021-04-16 09:27:34 +0200

    Added social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,36 +5,37 @@ import '../css/Footer.css';
 import home from '../assets/home.svg';
 import phone from '../assets/phone.svg';
 import mail from '../assets/mail.svg';
+import facebook from '../assets/facebook.svg';
+import instagram from '../assets/instagram.svg';
+import linkedin from '../assets/linkedin.svg';
 
 const Footer = () => (
   <footer>
     <section className="contact">
-      <div>
+      <a href="https://goo.gl/maps/8TbRsz5t7V4P7SBV6" target="_blank" rel="noreferrer">
         <img src={home} alt="home" />
-        <a href="https://goo.gl/maps/8TbRsz5t7V4P7SBV6" target="_blank" rel="noreferrer">Sneeuwvlerk 18, 8802DB Franeker</a>
-      </div>
-      <div>
+        <span>Sneeuwvlerk 18, 8802DB Franeker</span>
+      </a>
+      <a href="tel:0613540037">
         <img src={phone} alt="phone" />
-        <a href="tel:0613540037">06 13 54 00 37</a>
-      </div>
-      <div>
+        <span>06 13 54 00 37</span>
+      </a>
+      <a href="mailto:bart@bbcoachen.nl?subject=Coach Expeditie">
         <img src={mail} alt="mail" />
-        <a href="mailto:bart@bbcoachen.nl?subject=Coach Expeditie">bart@bbcoachen.nl</a>
-      </div>
+        <span>bart@bbcoachen.nl</span>
+      </a>
     </section>
-    {/* <nav>
-      <NavLink to="/privacy">
-        Privacyverklaring
-      </NavLink>
-      &middot;
-      <NavLink to="/terms">
-        Algemene voorwaarden
-      </NavLink>
-      &middot;
-      <NavLink to="/cookies">
-        Cookies
-      </NavLink>
-    </nav> */}
+    <section className="contact">
+      <a href="https://www.facebook.com/bbcoachen" target="_blank" rel="noreferrer">
+        <img src={facebook} alt="facebook" />
+      </a>
+      <a href="https://www.instagram.com/bbcoachen" target="_blank" rel="noreferrer">
+        <img src={instagram} alt="instagram" />
+      </a>
+      <a href="https://www.linkedin.com/company/gewooncoach/" target="_blank" rel="noreferrer">
+        <img src={linkedin} alt="linkedin" />
+      </a>
+    </section>
     <p>&copy; Copyright BB Coachen {new Date().getFullYear()} &middot; Realisatie <a href="https://sjaakluthart.nl" target="_blank" rel="noreferrer">Sjaak Luthart</a></p>
   </footer>
 );
